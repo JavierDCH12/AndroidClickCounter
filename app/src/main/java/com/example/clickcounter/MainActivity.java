3315f3928f376fe0ca563b130e979f62cb67e58c
JavierDCH12 <javierdelgadoch95@gmail.com>
2024-09-22 12:59:40 +0200

    countdown added

--- app/src/main/java/com/example/clickcounter/MainActivity.java
+++ app/src/main/java/com/example/clickcounter/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.clickcounter;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 
 import androidx.activity.EdgeToEdge;
@@ -15,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
 
     public static int contador;
+    private CountDownTimer countDownTimer;
+    private boolean isTimerRunning = false;
 
 
     @Override
@@ -31,8 +34,10 @@ public class MainActivity extends AppCompatActivity {
         binding.BotonClick.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                contador++;
-                binding.ContadorNumerico.setText(String.valueOf(contador));
+                if(isTimerRunning){
+                    contador++;
+                    binding.ContadorNumerico.setText(String.valueOf(contador));
+                }
             }
         });
 
@@ -45,25 +50,39 @@ public class MainActivity extends AppCompatActivity {
                 binding.ContadorNumerico.setText(String.valueOf(contador));
             }
         });
+    }
 
 
+    //CountDown
 
+    private void startCountdown(){
 
+        countDownTimer = new CountDownTimer(20000, 1000){
+            @Override
+            public void onTick(long millisUntilFinished) {
+                binding.CountDown.setText(String.valueOf(millisUntilFinished/1000));
+                isTimerRunning=true;
+            }
 
-
-
-
-
-
-
-
-
-
+            @Override
+            public void onFinish() {
+                isTimerRunning=false;
+                binding.CountDown.setText(String.valueOf("FIN"));
+                binding.BotonClick.setEnabled(false);
+            }
+        }.start();
 
     }
 
 
+    private void restart(){
+        if(countDownTimer!=null){
+            countDownTimer.cancel();
 
+        }
+        binding.BotonClick.setEnabled(true);
+        startCountdown();
+    }
 
 
 
