c6e0e7117399d14e01a8882c0d707d6b5760d5fb
JavierDCH12 <javierdelgadoch95@gmail.com>
2024-09-22 12:42:11 +0200

    click count added

--- app/src/main/java/com/example/clickcounter/MainActivity.java
+++ app/src/main/java/com/example/clickcounter/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.clickcounter;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,12 +21,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         contador=0;
 
 
 
+        //BOTON CLICK
+        binding.BotonClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                contador++;
+                binding.ContadorNumerico.setText(String.valueOf(contador));
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
